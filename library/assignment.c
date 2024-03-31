5418ec6833e01555e7e0c23699e49cab7e08ae41
TrulyBright <trulybright@yonsei.ac.kr>
2024-04-01 05:28:22 +0800

    assignment.h

---
+++ library/assignment.c
@@ -0,0 +1,12 @@
+#include "library/syscalls.h"
+#include "assignment.h"
+
+void runForSeconds(int seconds) {
+    unsigned int startTime;
+    syscall_system_time(&startTime);
+    unsigned int timeElapsed;
+    do {
+        syscall_system_time(&timeElapsed);
+        timeElapsed -= startTime;
+    } while (timeElapsed < seconds);
+}
---
+++ user/process1.c
@@ -0,0 +1,6 @@
+#include "assignment.h"
+
+int main(void) {
+    runForSeconds(5);
+    return 0;
+}
