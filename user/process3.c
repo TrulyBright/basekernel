e6829e485140d96a0a575a47aba066386c004398
TrulyBright <trulybright@yonsei.ac.kr>
2024-04-02 20:17:52 +0800

    process 2, 3, 4, 5 & schedulertest

---
+++ user/process3.c
@@ -0,0 +1,6 @@
+#include "library/assignment.h"
+
+int main(void) {
+    runForSeconds(5);
+    return 0;
+}
---
+++ user/schedulertest.c
@@ -0,0 +1,26 @@
+#include "kernel/types.h"
+#include "library/syscalls.h"
+#define NULL 0
+
+typedef struct {
+    const char *path;
+    const uint32_t pri;
+} ExecInfo;
+
+int main(void) {
+    const ExecInfo a[] = {
+        {"/bin/process1.exe", 9},
+        {"/bin/process2.exe", 7},
+        {"/bin/process3.exe", 2},
+        {"/bin/process4.exe", 1},
+        {"/bin/process5.exe", 5},
+    };
+    const int size = (int) (sizeof(a) / sizeof(a[0]));
+    int pfd[size];
+    for (int i = 0; i < size; i++)
+        pfd[i] = syscall_open_file(KNO_STDDIR, a[i].path, 0, 0);
+    for (int i = 0; i < size; i++)
+        syscall_process_run(pfd[i], 0, NULL);
+    for (int i = 0; i < size; i++)
+        syscall_object_close(pfd[i]);
+}
