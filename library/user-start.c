394cd154c2ffa665b99db10b367ba7e4dbf0c4ce
TrulyBright <trulybright@yonsei.ac.kr>
2024-04-15 03:25:00 +0800

    HELLO and BYE

--- library/user-start.c
+++ library/user-start.c
@@ -20,6 +20,7 @@ int main(int argc, const char *argv[]);
 
 void _start(int argc, const char **argv)
 {
-	printf("I'm a process with PID %d and PRI %d!\n", syscall_process_self(), syscall_process_pri());
+	printf("HELLO! I'm a process with PID %d and PRI %d!\n", syscall_process_self(), syscall_process_pri());
 	syscall_process_exit(main(argc, argv));
+	printf("BYE! I was a process with PID %d and PRI %d!\n", syscall_process_self(), syscall_process_pri());
 }
