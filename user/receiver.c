c18dce92b35f14c901c215fd3eb8b971bb463ab7
TrulyBright <trulybright@yonsei.ac.kr>
2024-04-21 01:09:57 +0800

    receiver tests failure

--- user/receiver.c
+++ user/receiver.c
@@ -1,11 +1,15 @@
 #include "library/string.h"
 #include "library/syscalls.h"
-#include "library/assignment.h"
 
 int main(const int argc, const char** argv) {
     int fd = syscall_make_named_pipe(argv[1]);
     if (fd < 0) {
         printf("Failed to create named pipe: %d\n", fd);
+        return 1;
+    }
+    int failing_fd = syscall_make_named_pipe(argv[1]);
+    if (failing_fd >= 0) {
+        printf("Named pipe creation should have failed\n");
         return 1;
     }
     const int content_size = strlen(argv[2]) + 1;
